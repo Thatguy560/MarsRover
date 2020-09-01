1a99b8df637850c61a4572934561b9fc74343de8
Peter Stevens <Peterstevens510@gmail.com>
2020-09-01 23:20:38 +0100

    Updating README.md, still in progress

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,7 +1,6 @@
 require 'Rover'
 
 describe Rover do
-
   it 'will let you create a new rover with any given co-ordinates and direction.' do
     rover = Rover.new(1, 2, 'N')
     expect(rover.x).to eq 1
@@ -19,13 +18,13 @@ describe Rover do
     it "won't let you move any further past the North plateau border" do
       rover = Rover.new(1, 3, 'N')
       10.times do
-        rover.move("M")
+        rover.move('M')
       end
       expect(rover.y).to eq Rover::PLATEAU_BORDERS[1]
     end
   end
 
-  context "For when the rover is facing East" do
+  context 'For when the rover is facing East' do
     it "will move one position in that direction if input is 'M' and direction is 'E'." do
       rover = Rover.new(1, 2, 'E')
       rover.move('M')
@@ -41,7 +40,7 @@ describe Rover do
     end
   end
 
-  context "For when the rover is facing South" do
+  context 'For when the rover is facing South' do
     it "will move one position in that direction if input is 'M' and direction is 'S'." do
       rover = Rover.new(2, 4, 'S')
       rover.move('M')
@@ -57,7 +56,7 @@ describe Rover do
     end
   end
 
-  context "For when the rover is facing West" do
+  context 'For when the rover is facing West' do
     it "will move one position in that direction if input is 'M' and direction is 'W'." do
       rover = Rover.new(4, 2, 'W')
       rover.move('M')
@@ -73,52 +72,52 @@ describe Rover do
     end
   end
 
-  context "For when the rover is turning left" do
-    it "will turn West when facing North" do
+  context 'For when the rover is turning left' do
+    it 'will turn West when facing North' do
       rover = Rover.new(3, 3, 'N')
       rover.turn('L')
       expect(rover.direction).to eq 'W'
     end
 
-    it "will turn North when facing East" do
+    it 'will turn North when facing East' do
       rover = Rover.new(3, 3, 'E')
       rover.turn('L')
       expect(rover.direction).to eq 'N'
     end
 
-    it "will turn East when facing South" do
+    it 'will turn East when facing South' do
       rover = Rover.new(3, 3, 'S')
       rover.turn('L')
       expect(rover.direction).to eq 'E'
     end
 
-    it "will turn South when facing West" do
+    it 'will turn South when facing West' do
       rover = Rover.new(3, 3, 'W')
       rover.turn('L')
       expect(rover.direction).to eq 'S'
     end
   end
 
-  context "For when the rover is turning right" do
-    it "will turn East when facing North" do
+  context 'For when the rover is turning right' do
+    it 'will turn East when facing North' do
       rover = Rover.new(3, 3, 'N')
       rover.turn('R')
       expect(rover.direction).to eq 'E'
     end
 
-    it "will turn South when facing East" do
+    it 'will turn South when facing East' do
       rover = Rover.new(3, 3, 'E')
       rover.turn('R')
       expect(rover.direction).to eq 'S'
     end
 
-    it "will turn West when facing South" do
+    it 'will turn West when facing South' do
       rover = Rover.new(3, 3, 'S')
       rover.turn('R')
       expect(rover.direction).to eq 'W'
     end
 
-    it "will turn North when facing West" do
+    it 'will turn North when facing West' do
       rover = Rover.new(3, 3, 'W')
       rover.turn('R')
       expect(rover.direction).to eq 'N'
@@ -127,6 +126,6 @@ describe Rover do
 
   it "will allow you to check the rover's current position at any given time." do
     rover = Rover.new(4, 4, 'S')
-    expect(rover.return_coordinates).to eq "Rover co-ordinates are 4, 4 facing S"
+    expect(rover.return_coordinates).to eq 'Rover co-ordinates are 4, 4 facing S'
   end
 end
