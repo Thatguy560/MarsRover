235965e942b6ec373a7ece8225c8999cea625f02
Peter Stevens <Peterstevens510@gmail.com>
2020-09-01 18:04:39 +0100

    turn method works for both right and left direction, added tests to check it works as how it should do, all tests pass

--- Lib/Rover.rb
+++ Lib/Rover.rb
@@ -1,18 +1,19 @@
 class Rover
   attr_accessor :x, :y, :direction
   PLATEAU_BORDERS = [0, 5].freeze
+
   LEFT = {
-    "N" => "W",
-    "E" => "N",
-    "S" => "E",
-    "W" => "S",
+    'N' => 'W',
+    'E' => 'N',
+    'S' => 'E',
+    'W' => 'S',
   }.freeze
 
   RIGHT = {
-    "N" => "E",
-    "E" => "S",
-    "S" => "W",
-    "W" => "N",
+    'N' => 'E',
+    'E' => 'S',
+    'S' => 'W',
+    'W' => 'N',
   }.freeze
 
   def initialize(x, y, direction)
@@ -21,12 +22,18 @@ class Rover
     @direction = direction
   end
 
-  def move(input) # Add plateau constraints to seperate class called Plateau?
-    @y += 1 if facing_north && moving(input) unless @y >= PLATEAU_BORDERS[1]
-    @x += 1 if facing_east && moving(input) unless @x >= PLATEAU_BORDERS[1]
-    @y -= 1 if facing_south && moving(input) unless @y <= PLATEAU_BORDERS[0]
-    @x -= 1 if facing_west && moving(input) unless @x <= PLATEAU_BORDERS[0]
-    return_coordinates
+  def move(input)
+    @y += 1 if (facing_north && input == 'M') unless @y >= PLATEAU_BORDERS[1]
+    @x += 1 if (facing_east && input == 'M') unless @x >= PLATEAU_BORDERS[1]
+    @y -= 1 if (facing_south && input == 'M') unless @y <= PLATEAU_BORDERS[0]
+    @x -= 1 if (facing_west && input == 'M') unless @x <= PLATEAU_BORDERS[0]
+    self
+  end
+
+  def turn(input) # Move turning to another class called Directions?
+    @direction = LEFT[@direction] if input == 'L'
+    @direction = RIGHT[@direction] if input == 'R'
+    self
   end
 
   def facing_north
@@ -45,17 +52,16 @@ class Rover
     @direction == 'W'
   end
 
-  def moving(input) # Change this to command method so if input is M OR R OR L.
-    input == 'M'
-  end
-
-  def turn(input) # Move turning to another class called Directions?
-    @direction = LEFT.fetch(@direction) if input == "L"
-    @direction = RIGHT.fetch(@direction) if input == "R"
-    "Rover is now facing #{@direction}"
-  end
-
-  def return_coordinates # Remove Method?
-    "Current Rover co-ordinates are #{@x}, #{@y} facing #{@direction}"
+  def return_coordinates
+    "Rover co-ordinates are #{@x}, #{@y} facing #{@direction}"
   end
 end
+
+# Change this to command method so if input is M OR R OR L.
+
+# def moving(input)
+  # input == 'M'
+# end
+
+# the lower-left coordinates of the plateau are 0,0.
+# the upper-right coordinates of the plateau are 5,5.
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,9 +1,8 @@
 require 'Rover'
 
 describe Rover do
-  # let(:rover) { Rover.new(x, y, direction) }
 
-  it 'will let you create a new rover at any point with any given co-ordinates and direction.' do
+  it 'will let you create a new rover with any given co-ordinates and direction.' do
     rover = Rover.new(1, 2, 'N')
     expect(rover.x).to eq 1
     expect(rover.y).to eq 2
@@ -26,7 +25,7 @@ describe Rover do
     end
   end
 
-  context 'For when the rover is facing East' do
+  context "For when the rover is facing East" do
     it "will move one position in that direction if input is 'M' and direction is 'E'." do
       rover = Rover.new(1, 2, 'E')
       rover.move('M')
@@ -36,13 +35,13 @@ describe Rover do
     it "won't let you move any further past the East plateau border" do
       rover = Rover.new(3, 1, 'E')
       10.times do
-        rover.move("M")
+        rover.move('M')
       end
       expect(rover.x).to eq Rover::PLATEAU_BORDERS[1]
     end
   end
 
-  context 'For when the rover is facing South' do
+  context "For when the rover is facing South" do
     it "will move one position in that direction if input is 'M' and direction is 'S'." do
       rover = Rover.new(2, 4, 'S')
       rover.move('M')
@@ -52,13 +51,13 @@ describe Rover do
     it "won't let you move any further past the South plateau border" do
       rover = Rover.new(1, 3, 'S')
       10.times do
-        rover.move("M")
+        rover.move('M')
       end
       expect(rover.y).to eq Rover::PLATEAU_BORDERS[0]
     end
   end
 
-  context 'For when the rover is facing West' do
+  context "For when the rover is facing West" do
     it "will move one position in that direction if input is 'M' and direction is 'W'." do
       rover = Rover.new(4, 2, 'W')
       rover.move('M')
@@ -68,14 +67,66 @@ describe Rover do
     it "won't let you move any further past the West plateau border" do
       rover = Rover.new(3, 1, 'W')
       10.times do
-        rover.move("M")
+        rover.move('M')
       end
       expect(rover.x).to eq Rover::PLATEAU_BORDERS[0]
     end
   end
 
+  context "For when the rover is turning left" do
+    it "will turn West when facing North" do
+      rover = Rover.new(3, 3, 'N')
+      rover.turn('L')
+      expect(rover.direction).to eq 'W'
+    end
+
+    it "will turn North when facing East" do
+      rover = Rover.new(3, 3, 'E')
+      rover.turn('L')
+      expect(rover.direction).to eq 'N'
+    end
+
+    it "will turn East when facing South" do
+      rover = Rover.new(3, 3, 'S')
+      rover.turn('L')
+      expect(rover.direction).to eq 'E'
+    end
+
+    it "will turn South when facing West" do
+      rover = Rover.new(3, 3, 'W')
+      rover.turn('L')
+      expect(rover.direction).to eq 'S'
+    end
+  end
+
+  context "For when the rover is turning right" do
+    it "will turn East when facing North" do
+      rover = Rover.new(3, 3, 'N')
+      rover.turn('R')
+      expect(rover.direction).to eq 'E'
+    end
+
+    it "will turn South when facing East" do
+      rover = Rover.new(3, 3, 'E')
+      rover.turn('R')
+      expect(rover.direction).to eq 'S'
+    end
+
+    it "will turn West when facing South" do
+      rover = Rover.new(3, 3, 'S')
+      rover.turn('R')
+      expect(rover.direction).to eq 'W'
+    end
+
+    it "will turn North when facing West" do
+      rover = Rover.new(3, 3, 'W')
+      rover.turn('R')
+      expect(rover.direction).to eq 'N'
+    end
+  end
+
   it "will allow you to check the rover's current position at any given time." do
     rover = Rover.new(4, 4, 'S')
-    expect(rover.return_coordinates).to eq "Current Rover co-ordinates are 4, 4 facing S"
+    expect(rover.return_coordinates).to eq "Rover co-ordinates are 4, 4 facing S"
   end
 end
